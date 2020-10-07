fafbcf7eda2871812d4659119b09c0d07f7dc2c2
GDC6 <GDC6@DESKTOP-201A8KJ>
2020-10-07 10:45:37 +0900

    초기 커밋.

---
+++ calculator1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace calculator1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sum = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Cross = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Sum
+            // 
+            this.Sum.Location = new System.Drawing.Point(387, 74);
+            this.Sum.Name = "Sum";
+            this.Sum.Size = new System.Drawing.Size(110, 23);
+            this.Sum.TabIndex = 0;
+            this.Sum.Text = "+";
+            this.Sum.UseVisualStyleBackColor = true;
+            this.Sum.Click += new System.EventHandler(this.Sum_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(387, 125);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(110, 23);
+            this.Minus.TabIndex = 1;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // Cross
+            // 
+            this.Cross.Location = new System.Drawing.Point(387, 176);
+            this.Cross.Name = "Cross";
+            this.Cross.Size = new System.Drawing.Size(110, 23);
+            this.Cross.TabIndex = 2;
+            this.Cross.Text = "*";
+            this.Cross.UseVisualStyleBackColor = true;
+            this.Cross.Click += new System.EventHandler(this.Cross_Click);
+            // 
+            // Division
+            // 
+            this.Division.Location = new System.Drawing.Point(387, 230);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(110, 23);
+            this.Division.TabIndex = 3;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = true;
+            this.Division.Click += new System.EventHandler(this.Division_Click);
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(29, 75);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(114, 23);
+            this.num1.TabIndex = 4;
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(201, 75);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(108, 23);
+            this.num2.TabIndex = 5;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(574, 76);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 23);
+            this.result.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 293);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Cross);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Sum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Sum;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Cross;
+        private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox result;
+    }
+}
+
---
+++ calculator1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sum_Click(object sender, EventArgs e)
+        {
+            int number1 = 0;
+            int number2 = 0;
+            
+            if (String.IsNullOrWhiteSpace(num1.Text))
+            {
+                MessageBox.Show(" 첫번째 칸에 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+                return;
+            }
+            else if(int.TryParse(num1.Text,out number1) == false)
+            {
+                MessageBox.Show(" 첫번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+            
+            if(String.IsNullOrWhiteSpace(num2.Text))
+            {
+                MessageBox.Show(" 두번째 칸에 숫자를 넣어주세요");
+                num2.Focus();
+                num2.SelectAll();
+                return;
+            }
+            else if (int.TryParse(num2.Text, out number2) == false)
+            {
+                MessageBox.Show(" 두번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+
+           number1 = Convert.ToInt32(num1.Text);
+           number2 = Convert.ToInt32(num2.Text);
+            int sum=SumResult(number1, number2);
+
+            result.Text = sum.ToString();
+        }
+
+        public int SumResult(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+            int number1 = 0;
+            int number2 = 0;
+
+            if (String.IsNullOrWhiteSpace(num1.Text))
+            {
+                MessageBox.Show(" 첫번째 칸에 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+                return;
+            }
+            else if (int.TryParse(num1.Text, out number1) == false)
+            {
+                MessageBox.Show(" 첫번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+
+            if (String.IsNullOrWhiteSpace(num2.Text))
+            {
+                MessageBox.Show(" 두번째 칸에 숫자를 넣어주세요");
+                num2.Focus();
+                num2.SelectAll();
+                return;
+            }
+            else if (int.TryParse(num2.Text, out number2) == false)
+            {
+                MessageBox.Show(" 두번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+
+
+            number1 = Convert.ToInt32(num1.Text);
+            number2 = Convert.ToInt32(num2.Text);
+            int minus = MinusResult(number1, number2);
+
+            result.Text = minus.ToString();
+        }
+        public int MinusResult(int number1, int number2)
+        {
+            int minus = number1 - number2;
+            return minus;
+        }
+
+        private void Cross_Click(object sender, EventArgs e)
+        {
+            int number1 = 0;
+            int number2 = 0;
+
+            
+
+            if (String.IsNullOrWhiteSpace(num1.Text))
+            {
+                MessageBox.Show(" 첫번째 칸에 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+                return;
+            }
+            else if (int.TryParse(num1.Text, out number1) == false)
+            {
+                MessageBox.Show(" 첫번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+
+            if (String.IsNullOrWhiteSpace(num2.Text))
+            {
+                MessageBox.Show(" 두번째 칸에 숫자를 넣어주세요");
+                num2.Focus();
+                num2.SelectAll();
+                return;
+            }
+            else if (int.TryParse(num2.Text, out number2) == false)
+            {
+                MessageBox.Show(" 두번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+
+
+            number1 = Convert.ToInt32(num1.Text);
+            number2 = Convert.ToInt32(num2.Text);
+            int cross = CrossResult(number1, number2);
+
+            result.Text = cross.ToString();
+        }
+        public int CrossResult(int number1, int number2)
+        {
+            int Cross = number1 * number2;
+            return Cross;
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+
+            int number1 = 0;
+            int number2 = 0;
+
+            if (String.IsNullOrWhiteSpace(num1.Text))
+            {
+                MessageBox.Show(" 첫번째 칸에 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+                return;
+            }
+            else if (int.TryParse(num1.Text, out number1) == false)
+            {
+                MessageBox.Show(" 첫번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+
+            if (String.IsNullOrWhiteSpace(num2.Text))
+            {
+                MessageBox.Show(" 두번째 칸에 숫자를 넣어주세요");
+                num2.Focus();
+                num2.SelectAll();
+                return;
+            }
+            else if (int.TryParse(num2.Text, out number2) == false)
+            {
+                MessageBox.Show(" 두번째 칸에 문자가 들어갔습니다. 숫자를 넣어주세요");
+                num1.Focus();
+                num1.SelectAll();
+            }
+
+            number1 = Convert.ToInt32(num1.Text);
+            number2 = Convert.ToInt32(num2.Text);
+            int division =DivisionResult(number1, number2);
+
+            result.Text = division.ToString();
+        }
+
+        public int DivisionResult(int number1, int number2)
+        {
+            int Division = number1 / number2;
+            return Division;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
